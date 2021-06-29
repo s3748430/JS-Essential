a56c99e2460dbe56b31693a62533db9ec6912d8b
s3748430 <62051704+s3748430@users.noreply.github.com>
2021-06-29 07:38:05 +0700

    commit

---
+++ Challenge/book.js
@@ -0,0 +1,19 @@
+class Books {
+  constructor(
+    name,
+    author,
+    gender,
+    publishDate,
+    Language,
+    rate,
+    dateAccquired
+  ) {
+    this.name = name;
+    this.author = author;
+    this.gender = gender;
+    this.publishDate = publishDate;
+    this.Language = Language;
+    this.rate = rate;
+    this.dateAccquired = dateAccquired;
+  }
+}
