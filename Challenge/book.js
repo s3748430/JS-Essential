58e6c119176788f5a2501ea36166aa858158425b
s3748430 <62051704+s3748430@users.noreply.github.com>
2021-06-29 23:27:35 +0700

    commit

--- Challenge/book.js
+++ Challenge/book.js
@@ -16,4 +16,16 @@ class Books {
     this.rate = rate;
     this.dateAccquired = dateAccquired;
   }
+  newAuthor(newAuthor) {
+    this.author = newAuthor;
+  }
+  BookAccquired() {
+    let now = new Date();
+    let acquired = new Date(this.dateAcquired);
+    let elapsed = now - acquired; // elapsed time in milliseconds
+    let daysSinceAcquired = Math.floor(elapsed / (1000 * 3600 * 24));
+    return daysSinceAcquired;
+  }
 }
+
+export default Books;
---
+++ Challenge/script.js
@@ -0,0 +1,37 @@
+import Book from "./book.js";
+
+const Inferno = new Book(
+  "Inferno",
+  "Dan Brown",
+  "Fiction",
+  "2016",
+  "English",
+  "R-18",
+  "1 December 2018"
+);
+
+console.log(Inferno);
+
+const AngelAndDemon = new Book(
+  "Angel and Demon",
+  "Dan Brown",
+  "Fiction",
+  "2012",
+  "Enlish",
+  "R-18",
+  "1 December 2019"
+);
+
+console.log(AngelAndDemon);
+
+const DaVinciCode = new Book(
+  "Da Vinci Code",
+  "Dan Brown",
+  "Fiction",
+  "2010",
+  "English",
+  "R-18",
+  "1 December 2020"
+);
+
+console.log(DaVinciCode);
